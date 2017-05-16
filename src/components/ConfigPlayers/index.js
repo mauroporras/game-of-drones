48befaba6f527feb9863244a15e293b03effab75
Mauro Porras P <mauroporrasp@gmail.com>
2017-05-16 11:02:24 -0500

    wip

--- src/components/ConfigPlayers/index.js
+++ src/components/ConfigPlayers/index.js
@@ -3,18 +3,39 @@ import React from 'react'
 import './styles.css'
 
 const ConfigPlayers = () => (
-  <fieldset>
-    <legend>Enter your names</legend>
+  <fieldset className='ConfigPlayers'>
+    <legend className='ConfigPlayers__legend'>Configure Players</legend>
     <form>
-      <div>
-        <label htmlFor='player1-name'>Player 1</label>
-        <input type='text' id='player1-name' />
+      <div className='ConfigPlayers__section'>
+        <label
+          className='ConfigPlayers__label'
+          htmlFor='player1-name'
+        >
+          Player 1
+        </label>
+        <input
+          autofocus
+          className='ConfigPlayers__input-text'
+          id='player1-name'
+          placeholder="Player 1's name"
+          type='text'
+        />
       </div>
-      <div>
-        <label htmlFor='player2-name'>Player 2</label>
-        <input type='text' id='player2-name' />
+      <div className='ConfigPlayers__section'>
+        <label
+          className='ConfigPlayers__label'
+          htmlFor='player2-name'
+        >
+          Player 2
+        </label>
+        <input
+          className='ConfigPlayers__input-text'
+          id='player2-name'
+          placeholder="Player 2's name"
+          type='text'
+        />
       </div>
-      <div>
+      <div className='ConfigPlayers__section'>
         <button type='submit'>Play!</button>
       </div>
     </form>
