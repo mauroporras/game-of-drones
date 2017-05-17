2c39d98bf26ddafe1c06e825a74ac73dea795890
Mauro Porras P <mauroporrasp@gmail.com>
2017-05-16 23:58:22 -0500

    wip

--- src/components/Match.js
+++ src/components/Match.js
@@ -3,57 +3,87 @@ import React from 'react'
 import GameScreen from './GameScreen/'
 import Round from '../containers/Round/'
 
-const state = {
-  players: [
-    {
-      name: 'nata',
-      roundsWon: 0,
-    },
-    {
-      name: 'mauro',
-      roundsWon: 0,
-    },
-  ],
-  currentRound: 1,
-  rules: {
-    roundsWonToWinMatch: 3,
-    moves: [
-      {
-        id: 'rock',
-        beats: ['scissors'],
+class Match extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentRound: 0,
+      roundsElements: [],
+    }
+
+    this._roundDone = this._roundDone.bind(this)
+  }
+
+  componentDidMount() {
+    this._addRound()
+  }
+
+  _addRound() {
+    const state = {
+      players: [
+        {
+          name: 'Nata',
+          roundsWon: 0,
+        },
+        {
+          name: 'Mauro',
+          roundsWon: 0,
+        },
+      ],
+      rules: {
+        roundsWonToWinMatch: 3,
+        moves: [
+          {
+            id: 'rock',
+            beats: ['scissors'],
+          },
+          {
+            id: 'paper',
+            beats: ['rock'],
+          },
+          {
+            id: 'scissors',
+            beats: ['paper'],
+          },
+        ]
       },
-      {
-        id: 'paper',
-        beats: ['rock'],
-      },
-      {
-        id: 'scissors',
-        beats: ['paper'],
-      },
-    ]
-  },
+    }
+
+    const { moves } = state.rules
+    const { players } = state
+    this.setState({
+      currentRound: this.state.currentRound + 1
+    }, () => {
+      const roundNumber = this.state.currentRound
+
+      this.setState({
+        roundsElements: [
+          ...this.state.roundsElements,
+          <Round
+            key={roundNumber}
+            moves={moves}
+            onDone={this._roundDone}
+            players={players}
+            roundNumber={roundNumber}
+          />
+        ]
+      })
+    })
+  }
+
+  _roundDone(winner) {
+    console.info(winner)
+    this._addRound()
+  }
+
+  render() {
+    return (
+      <GameScreen title={'Match in Progress'}>
+        {this.state.roundsElements}
+      </GameScreen>
+    )
+  }
 }
-
-const roundsElements = []
-
-const { moves } = state.rules
-const { players } = state
-const roundNumber = state.currentRound
-
-roundsElements.push(
-  <Round
-    key={roundNumber}
-    moves={moves}
-    onDone={winner => console.info(winner)}
-    players={players}
-    roundNumber={roundNumber}
-  />
-)
-
-const Match = () => (
-  <GameScreen title={'Match in Progress'}>
-    {roundsElements}
-  </GameScreen>
-)
 
 export default Match
